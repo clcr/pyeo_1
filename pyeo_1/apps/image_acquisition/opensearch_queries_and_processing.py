4d68aa6c20c923daee53103cebcec3e54b59a588
Simon <simon.dzurjak@protonmail.com>
2023-05-11 11:30:07 +0100

    removed token line

--- pyeo_1/apps/image_acquisition/opensearch_queries_and_processing.py
+++ pyeo_1/apps/image_acquisition/opensearch_queries_and_processing.py
@@ -32,7 +32,6 @@ REFRESH_TOKEN_URL = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/p
 
 USERNAME = CONFIG["authentication"]["username"]
 PASSWORD = CONFIG["authentication"]["password"]
-REFRESH_TOKEN = CONFIG["tokens"]["refresh"]
 
 SAFE_DOWNLOAD_PATH = CONFIG["directories"]["l2a_safes"]
 MIN_IMAGE_SIZE = CONFIG["sentinel2_properties"]["min_image_size"]
